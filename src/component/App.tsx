856596de172c701a4d1f2c5ffc286254a6f0a0c3
Serban Ghita <sghita@adobe.com>
2018-03-26 17:41:47 +0300

    Added example of changing state via events

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -10,30 +10,43 @@ export interface IAppState {
   titleColor: string
 }
 
+interface IFeedbackBtnProps {
+  label: string;
+}
+
+interface IFeedbackBtnState {
+  isOpened: boolean;
+}
+
+class FeedbackBtn extends React.Component<IFeedbackBtnProps, IFeedbackBtnState> {
+
+  constructor(props) {
+    super(props);
+    this.state = {isOpened: false};
+  }
+
+
+  public render() {
+    const handleBtnClick = () => {
+      this.setState((oldState: IFeedbackBtnState, props: IFeedbackBtnProps) => {
+        return {isOpened: !oldState.isOpened}
+      });
+    };
+
+    return (
+      <button onClick={handleBtnClick}>
+        {this.props.label} <span>{this.state.isOpened ? "↓" : "[+]"}</span>
+      </button>
+    );
+  }
+
+}
+
 export default class App extends React.Component<IAppProps, IAppState> {
 
   constructor(props: IAppProps) {
     super(props);
     this.state = {title: props.title, titleColor: props.titleColor};
-  }
-
-  public componentWillMount() {
-    this.setState({titleColor: "yellow"}, () => {
-      console.log("Set state in componentWillMount().");
-    });
-  }
-
-  public componentDidMount() {
-    this.setState({titleColor: "green"}, () => {
-      console.log("Set state in componentDidMount().");
-    });
-
-    // For later.
-    setTimeout(() => {
-      this.setState({titleColor: this.props.titleColor}, () => {
-        console.log("Set state in componentDidMount() - lazy");
-      });
-    }, 2500);
   }
 
   public render() {
@@ -45,6 +58,7 @@ export default class App extends React.Component<IAppProps, IAppState> {
         <div>
             <h1 style={pStyle}>{this.props.title}</h1>
             <p>A static description of my app.</p>
+            <FeedbackBtn label="Send feedback"/>
         </div>
     );
   }
