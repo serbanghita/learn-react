4988b69d01143be33e776cc08f8c7fb526d9611b
Serban Ghita <sghita@adobe.com>
2018-03-27 15:22:48 +0300

    Lifting the local state

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import Feedback from "./Feedback";
+import CurrencyConverter from "./CurrencyConverter";
 
 export interface IAppProps {
     title: string;
@@ -8,66 +10,6 @@ export interface IAppProps {
 export interface IAppState {
   title: string,
   titleColor: string
-}
-
-interface IFeedbackBtnProps {
-  label: string;
-}
-
-interface IFeedbackBtnState {
-  isOpened: boolean;
-}
-
-class FeedbackBtn extends React.Component<IFeedbackBtnProps, IFeedbackBtnState> {
-
-  constructor(props) {
-    super(props);
-    this.state = {isOpened: false};
-  }
-
-
-  public render() {
-    const handleBtnClick = () => {
-      this.setState((oldState: IFeedbackBtnState, props: IFeedbackBtnProps) => {
-        return {isOpened: !oldState.isOpened}
-      });
-    };
-
-    const formRatings = [1, 2, 3, 4, 5].map((number) =>
-      <span key={number.toString()}>
-        <input type="radio" name="rating" value={number} />
-        <label>{number}</label>
-      </span>
-    );
-
-    const form = <form>
-      <div>
-        <label>Name</label> <br />
-        <input type="text" name="name" placeholder="Your name" />
-      </div>
-      <div>
-        <label>Message</label> <br />
-        <textarea name="message" placeholder="Your message ..." />
-      </div>
-      <div>
-        <label>Rate page</label> <br />
-        {formRatings}
-      </div>
-      <div>
-        <button>Send</button>
-      </div>
-    </form>;
-
-    return (
-      <div>
-        <button onClick={handleBtnClick}>
-          {this.props.label} <span>{this.state.isOpened ? "↓" : "[+]"}</span>
-        </button>
-        {this.state.isOpened ? form : ""}
-      </div>
-    );
-  }
-
 }
 
 export default class App extends React.Component<IAppProps, IAppState> {
@@ -86,7 +28,8 @@ export default class App extends React.Component<IAppProps, IAppState> {
         <div>
             <h1 style={pStyle}>{this.props.title}</h1>
             <p>A static description of my app.</p>
-            <FeedbackBtn label="Send feedback"/>
+            <Feedback label="Send feedback"/>
+            <CurrencyConverter />
         </div>
     );
   }
