ebeb38dba61ab86c89a7f8cba741ee15956e5a0f
Serban Ghita <sghita@adobe.com>
2018-03-28 12:40:51 +0300

    Formatting code

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./component/App";
 
-ReactDOM.render(<App title="React and TypeScript demo!" titleColor="blue" />, document.getElementById("app"));
+ReactDOM.render(
+  <App title="React and TypeScript demo!" titleColor="blue" />,
+  document.getElementById("app")
+);
