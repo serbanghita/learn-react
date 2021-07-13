96d89b2f9701921f75a2e9b493f51ff408d499c5
Serban Ghita <sghita@adobe.com>
2021-07-13 12:07:09 +0300

    Added unsubscribe scenario.

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ export default class App extends React.Component<{}, {}> {
   public render() {
 
     const w = new World();
-    w.addPlayer("a", {x: 100, y: 101}, 10, 100);
-    w.addPlayer("b", {x: 200, y: 201}, 11, 100);
+    w.addPlayer("p1", {x: 100, y: 101}, 10, 100);
+    w.addPlayer("p2", {x: 200, y: 201}, 11, 100);
     w.run();
 
     return (
--- src/World.ts
+++ src/World.ts
@@ -1,25 +1,52 @@
 import Player from "./Player";
+import {Unsubscribe} from "redux";
 
 export default class World {
   private entities: Player[];
+  private listeners: { playerId: string, listenerFn: Unsubscribe };
 
   constructor() {
     this.entities = [];
+    this.listeners = Object.create(null);
   }
 
   public addPlayer(id, position, speed, health) {
     const player = new Player(id, position, speed, health);
     this.entities.push(player);
-    // World subscribes to Player updates.
-    player.stateStore.subscribe(() => console.log(`update ${player.id}`, player.stateStore.getState()));
+    this.subscribeToPlayer(player);
   }
 
-  public getPlayer(id): Player | undefined {
-    return this.entities.find(entity => entity.id === id);
+  private subscribeToPlayer(player: Player) {
+    // World subscribes to Player updates.
+    const playerListener = player.stateStore.subscribe(() => console.log(`update ${player.id}`, player.stateStore.getState()));
+    // Save the listener so we can unsub later.
+    this.listeners[player.id] = playerListener;
+  }
+
+  private unsubscribeToPlayer(player: Player) {
+    this.listeners[player.id]();
+    delete this.listeners[player.id];
+  }
+
+  public getPlayer(id): Player {
+    const player = this.entities.find(entity => entity.id === id);
+    if (!player) {
+      throw new Error(`No player found with id: ${id}`);
+    }
+
+    return player;
   }
 
   run() {
-    this.getPlayer("a")?.move();
-    this.getPlayer("a")?.move();
+    const p1 = this.getPlayer("p1");
+    const p2 = this.getPlayer("p2");
+
+    p1.move();
+    p1.move();
+
+    p2.move();
+    this.unsubscribeToPlayer(p2)
+
+    p2.move();
   }
 }
